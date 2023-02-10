1bc1e8691f5afa5c18a8c8d4a25975ac697eafbc
pasecolra <paulcolmenares@colegioguanenta.edu.co>
2023-02-10 09:38:54 -0500

    se agrega el diagrama de flujo y el archivo conversor_temperatura.py

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -1,2 +1,14 @@
-# convertir temperatura
-#
+# conversor de temperatura 
+
+# int put 
+
+C = int(input("digite el numero de C: "))
+
+#proccesing
+
+F = (C*1.8 + 32)
+K = (C+273.15)
+ 
+print("la convercion a farenhell es " + str(F))
+print("la convercion a kelvin es " + str(K))
+
